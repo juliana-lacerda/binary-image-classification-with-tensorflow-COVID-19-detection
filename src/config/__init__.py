2b46a1ed9a62b6bf9f880e4cf90cd7020d69777e
Juliana Lacerda <juliana.lacerda@grupoboticario.com.br>
2023-09-22 17:56:00 -0300

    chore: change variable name

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,7 +2,7 @@ import yaml
 import os
 
 # Open the YAML configuration file
-script_dit= os.path.dirname(__file__)
-yaml_file = os.path.join(script_dit, 'config.yaml')
-with open(yaml_file , 'r') as file:
+script_dir = os.path.dirname(__file__)
+yaml_file = os.path.join(script_dir, "config.yaml")
+with open(yaml_file, "r") as file:
     cf = yaml.safe_load(file)
